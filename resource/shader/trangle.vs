cfcfbc53ac5a192b49e8be62ff2a006d1c4facbd
xietao <3045153794@qq.com>
2022-05-16 14:54:13 +0800

    add vendor

--- resource/shader/trangle.vs
+++ resource/shader/trangle.vs
@@ -1,5 +1,5 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 a_Pos;
 layout (location = 1) in vec2 a_coord;
 out vec2 tex_coord;
 uniform mat4 model;
@@ -7,6 +7,6 @@ uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
-  gl_Position = projection * view * model * vec4(aPos, 1.0);
+  gl_Position = projection * view * model * vec4(a_Pos, 1.0);
   tex_coord = a_coord;
 }
--- src/model/Trangle.cpp
+++ src/model/Trangle.cpp
@@ -5,25 +5,26 @@ Trangle::Trangle(Shader shader, Texture2D texture_2d)
   this->shader = shader;
   this->texture_2d = texture_2d;
   set_buffers();
+  std::cout<<"create Trangle\n";
 }
 
 void Trangle::set_buffers() {
   GLfloat data[] ={
     -1.0, 0.0, -1.0, 0.0, 0.0,
      1.0, 0.0, -1.0, 1.0, 0.0,
-     1.0, 0.0,  1.0, 0.0, 0,0,
-    -1.0, 0.0,  1.0, 1.0, 0,0,
-     0.0, 1,0,  0.0, 0.5, 1.0
+     1.0, 0.0,  1.0, 0.0, 0.0,
+    -1.0, 0.0,  1.0, 1.0, 0.0,
+     0.0, 1.0,  0.0, 0.5, 1.0
   };
   GLuint idx[] = {
     0, 1, 4,
     1, 2, 4,
     2, 3, 4,
-    3, 4, 4,
+    3, 0, 4,
     0, 1, 2,
-    2, 3, 1
+    2, 3, 0
   };
-  glGenBuffers(1, &this->vao);
+  glGenVertexArrays(1, &this->vao);
   glBindVertexArray(this->vao);
   
   glGenBuffers(1, &this->vbo);
@@ -33,11 +34,13 @@ void Trangle::set_buffers() {
   glGenBuffers(1, &this->ebo);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->ebo);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(idx), idx, GL_STATIC_DRAW);
+  
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (void*)0);
   glEnableVertexAttribArray(0);
   
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (void*)(sizeof(GLfloat) * 2));
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (void*)(sizeof(GLfloat) * 3));
   glEnableVertexAttribArray(1);
+  
   glBindVertexArray(0);
 }
 
@@ -45,6 +48,8 @@ void Trangle::draw() {
   shader.use();
   glm::mat4 model = glm::mat4(1.0);
   model = glm::translate(model, this->position);
+  model = glm::rotate(model, glm::radians(30.0f),glm::vec3(0.0, 1.0, 0.0));
+  this->shader.set_matrix4("model", model);
   glBindVertexArray(this->vao);
   glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0);
   glBindVertexArray(0);
