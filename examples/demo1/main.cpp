7f383d4b9adc2a3ccac32993dea3cf5a21505af0
xietao <3045153794@qq.com>
2022-07-15 00:33:07 +0800

    pm

--- examples/demo1/main.cpp
+++ examples/demo1/main.cpp
@@ -2,7 +2,7 @@
 #include "Trangle.h"
 #include "TrackBallControls.h"
 #include "CameraFps.h"
-#include "RenderText.h"
+
 #include "Animation.hpp"
 #include "Animator.hpp"
 #include "model.hpp"
@@ -10,7 +10,7 @@
 const GLuint SCREEN_WIDTH = 800;
 const GLuint SCREEN_HEIGHT = 600;
 Trangle* trangle;
-RenderText* text;
+
 gl_cameras::Camera* camera_p;
 gl_cameras::CameraFps* camera_fps;
 gl_animation::Animation* animation;
@@ -29,12 +29,12 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void init() {
   ResourceManager::load_shader("../resource/shader/trangle.vs", "../resource/shader/trangle.fs","trangle");
-  ResourceManager::load_texture("../resource/texture/awesomeface.png", "trangle");
+  //ResourceManager::load_texture("../resource/texture/awesomeface.png", "trangle");
   ResourceManager::load_shader("../resource/shader/front.vs", "../resource/shader/front.fs", "front");
   ResourceManager::load_shader("../resource/shader/anim_model.vs", "../resource/shader/anim_model.fs", "anim");
-  model = new gl_animation::Model("../resource/objects/car/blender_car.glb","false");
-  animation = new gl_animation::Animation("../resource/objects/car/blender_car.glb", model);
-  animator = new gl_animation::Animator(animation);
+  model = new gl_animation::Model("../resource/objects/派蒙bk/untitled.obj","false");
+  // animation = new gl_animation::Animation("../resource/objects/派蒙bk/untitled.obj", model);
+  // animator = new gl_animation::Animator(animation);
 
   //camera_fps = new gl_cameras::CameraFps(glm::vec3(0.0, 0.0, 0.0), glm::vec3(1.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
   camera_p = new gl_cameras::Camera(45.0f, static_cast<float>(SCREEN_WIDTH)/static_cast<float>(SCREEN_HEIGHT), 0.1f, 1000.0f);
@@ -46,7 +46,7 @@ void init() {
   ResourceManager::get_shader("front").set_matrix4("projection", glm::ortho(0.0f, static_cast<GLfloat>(SCREEN_WIDTH), 0.0f, static_cast<GLfloat>(SCREEN_HEIGHT)), true);
   
   trangle = new Trangle(ResourceManager::get_shader("trangle"), ResourceManager::get_texture("trangle"));
-  text = new RenderText(ResourceManager::get_shader("front"));
+  //text = new RenderText(ResourceManager::get_shader("front"));
 }
 int main() {
   glfwInit();
@@ -83,7 +83,7 @@ int main() {
 
     processInput(window);
     gl_cameras::TrackBallControls::instance().update();
-    animator->update_animation(deltaTime);
+    // animator->update_animation(deltaTime);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     ResourceManager::get_shader("anim").use();
@@ -94,8 +94,8 @@ int main() {
     std::cout<<"c="<<camera_p->get_center().x<<" "<<camera_p->get_center().y<<" "<<camera_p->get_center().z<<std::endl;
     std::cout<<"u="<<camera_p->get_up().x<<" "<<camera_p->get_up().y<<" "<<camera_p->get_up().z<<std::endl;
    
-    auto transforms = animator->get_final_bone_matrices();
-    
+    // auto transforms = animator->get_final_bone_matrices();
+    /*
     for(int i = 0; i < transforms.size(); i++) {
       ResourceManager::get_shader("anim").set_matrix4(("finalBonesMatrices[" + std::to_string(i) + "]").c_str(), transforms[i], true);
       std::cout<<transforms[i][0][0]<<" "<<transforms[i][0][1]<<" "<<transforms[i][0][2]<<" "<<transforms[i][0][3]<<"\n";
@@ -105,16 +105,16 @@ int main() {
       std::cout<<"\n";
       //<<transforms[i][0]<<" "<<transforms[i][0]<<" "<<transforms[i][0]<<"\n";
     }
-
+    */
     glm::mat4 t_model = glm::mat4(1.0);
-    t_model = glm::translate(t_model, glm::vec3(0.0f, -0.4f, 0.0f));
-    t_model = glm::scale(t_model, glm::vec3(0.5f, 0.5f, 0.5f));
+    t_model = glm::translate(t_model, glm::vec3(0.0f, -1.0f, 0.0f));
+    t_model = glm::scale(t_model, glm::vec3(2.0f,2.0f, 2.0f));
     ResourceManager::get_shader("anim").set_matrix4("model", t_model, true);
     model->draw(ResourceManager::get_shader("anim"));
 
     //trangle->draw();
     glDisable(GL_DEPTH_TEST);
-    text->draw(L"东风谷早苗",650.0, 25.0, glm::vec3(1.0, 1.0, 1.0));
+    
     glEnable(GL_DEPTH_TEST);
     glfwSwapBuffers(window);
     glfwPollEvents();
