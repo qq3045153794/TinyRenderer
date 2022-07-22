1ed1431683aa450f96557531801bb75d633c4869
xietao <3045153794@qq.com>
2022-07-22 20:02:09 +0800

    window

---
+++ src/core/Window.cpp
@@ -0,0 +1,40 @@
+#include "core/Window.h"
+
+Window& Window::instand() {
+  static Window i;
+  return i;
+}
+
+Window::Window()
+  : m_width(800U),
+    m_height(600U),
+    m_x(0U),
+    m_y(0U),
+    title("LearnOpenGL") {
+
+}
+
+Window::~Window() {
+  if(m_window) {
+    glfwDestroyWindow(m_window);
+    glfwTerminate();
+  }
+}
+
+void Window::clear() {
+  if(m_window) {
+    glfwDestroyWindow(m_window);
+    glfwTerminate();
+  }
+}
+
+void Window::init() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  
+  m_window = glfwCreateWindow(m_width, m_height, title.c_str(), NULL, NULL);
+
+  glfwMakeContextCurrent(m_window);
+}
