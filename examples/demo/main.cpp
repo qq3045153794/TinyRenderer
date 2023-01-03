cdf3537deabb1d99d5c0c44187ea1201c7fbec37
xietao <xietao@minieye.cc>
2023-01-04 00:56:10 +0800

    添加GPU 支持纹理查询

--- examples/demo/main.cpp
+++ examples/demo/main.cpp
@@ -2,7 +2,26 @@
 #include "core/Log.h"
 
 int main() {
-  core::App::instand().init();
+  auto& app = core::App::instand();
+  app.init();
+
+  std::cout << "$ System Information" << '\n' << std::endl;
+  std::cout << "  GPU Vendor Name:   " << app.gl_vendor << std::endl;
+  std::cout << "  OpenGL Renderer:   " << app.gl_renderer << std::endl;
+  std::cout << "  OpenGL Version:    " << app.gl_version << std::endl;
+  std::cout << "  GLSL Core Version: " << app.glsl_version << '\n' << std::endl;
+
+  std::cout << "$ Maximum supported texture size: " << '\n' << std::endl;
+  std::cout << "  1D / 2D texture (width and height): " << app.gl_texsize << std::endl;
+  std::cout << "  3D texture (width, height & depth): " << app.gl_texsize_3d << std::endl;
+  std::cout << "  Cubemap texture (width and height): " << app.gl_texsize_cubemap << std::endl;
+  std::cout << "  Max number of texture units: " << app.gl_max_texture_units << '\n' << std::endl;
+
+  std::cout << "$ Maximum allowed number of uniform buffers: " << '\n' << std::endl;
+  std::cout << "  Vertex shader:   " << app.gl_maxv_ubos << std::endl;
+  std::cout << "  Geometry shader: " << app.gl_maxg_ubos << std::endl;
+  std::cout << "  Fragment shader: " << app.gl_maxf_ubos << std::endl;
+
   bool run = true;
   while (run) {
     core::App::instand().render_update();
--- src/core/App.cpp
+++ src/core/App.cpp
@@ -36,6 +36,26 @@ void App::init() {
     CORE_ERROR("Failed to initialize GLAD");
     return;
   }
+
+  gl_vendor = std::string(reinterpret_cast<const char*>(glGetString(GL_VENDOR)));
+  gl_renderer = std::string(reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
+  gl_version = std::string(reinterpret_cast<const char*>(glGetString(GL_VERSION)));
+  glsl_version =
+      std::string(reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION)));
+
+  // texture size limit, max number of texture units and image units
+  glGetIntegerv(GL_MAX_TEXTURE_SIZE, &gl_texsize);
+  glGetIntegerv(GL_MAX_3D_TEXTURE_SIZE, &gl_texsize_3d);
+  glGetIntegerv(GL_MAX_CUBE_MAP_TEXTURE_SIZE, &gl_texsize_cubemap);
+  glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &gl_max_texture_units);
+
+
+  // max number of uniform blocks in each shader stage
+  glGetIntegerv(GL_MAX_VERTEX_UNIFORM_BLOCKS, &gl_maxv_ubos);
+  glGetIntegerv(GL_MAX_GEOMETRY_UNIFORM_BLOCKS, &gl_maxg_ubos);
+  glGetIntegerv(GL_MAX_FRAGMENT_UNIFORM_BLOCKS, &gl_maxf_ubos);
+
+
   // 渲染器绑定场景
   Render::attach("Welcome Screen");
 }
@@ -51,7 +71,7 @@ void App::event_update() {
     Window::on_layer_switch();
     Input::set_key_down('\r', false);
   }
-  
+
   Clock::update();
   Render::draw_imGui();
 }
