d1cda6446bf04bc688c436ff771b7bb0107fbf49
xietao <3045153794@qq.com>
2023-01-11 00:13:55 +0800

    添加gtest单元测试库

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "component/Transform.h"
 #include <memory>
-
+#include "gtest/gtest.h"
 int main() {
-
+  EXPECT_EQ(1, 1) << "ok";
+  EXPECT_EQ(1, 2) << "faid";
   std::shared_ptr<component::Transform> T =std::make_shared<component::Transform>();
   auto& pos =  T->get_position();
   std ::cout << pos.x << pos.y << pos.z << std::endl;
---
+++ vendor/googletest/readme.md
@@ -0,0 +1,15 @@
+# 构建
+
+
+```bash
+git clone git@github.com:google/googletest.git
+
+cd googletest
+
+mkdir build && cd build
+
+cmake .. -DCMAKE_INSTALL_PREFIX=DIR # DIR 是安装路径
+
+make && make install 
+
+```
