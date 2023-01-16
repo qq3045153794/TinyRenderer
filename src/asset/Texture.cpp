cd8cd902c76530fcff9163f2d4f43d7af0d9a1d0
xietao <3045153794@qq.com>
2023-01-17 01:44:16 +0800

    hdr2cubemap

--- src/asset/Texture.cpp
+++ src/asset/Texture.cpp
@@ -49,9 +49,10 @@ Texture::Texture(const GLchar* path, int resolution) : m_target(GL_TEXTURE_CUBE_
   m_height = image.get_height();
   m_image_format = image.get_img_format();
   m_internal_format = image.get_ine_format();
-  // 创建2D texture
-  glGenTextures(1, &m_id);
-  glBindTexture(GL_TEXTURE_2D, m_id);
+  // 创建 hdr 2D texture
+  GLuint hdr_id;
+  glGenTextures(1, &hdr_id);
+  glBindTexture(GL_TEXTURE_2D, hdr_id);
   glTexImage2D(GL_TEXTURE_2D, 0, m_internal_format, m_width, m_height, 0, m_image_format,
                GL_FLOAT, image.get_buffer());
   glBindTexture(GL_TEXTURE_2D, 0);
@@ -60,7 +61,12 @@ Texture::Texture(const GLchar* path, int resolution) : m_target(GL_TEXTURE_CUBE_
 
   cubemap_fbo->set_depth_texture();
 
-  for (size_t i = 0; i < 6; i++) {
+  const size_t faces = 6;
+
+  // 创建cube
+  glGenTextures(1, &m_id);
+  glBindBuffer(m_target, m_id);
+  for (size_t i = 0; i < faces; i++) {
     glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, 
                  resolution, resolution, 0, GL_RGB, GL_FLOAT, nullptr);
   }
@@ -79,7 +85,89 @@ Texture::Texture(const GLchar* path, int resolution) : m_target(GL_TEXTURE_CUBE_
      glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 0.0f,  0.0f, -1.0f), glm::vec3(0.0f, -1.0f,  0.0f))
   };
 
+ float data[] = {
+    // back face
+    -1.0f, -1.0f, -1.0f,
+     1.0f,  1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f,  1.0f, -1.0f,
+    -1.0f, -1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    // front face
+    -1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
+    // left face
+    -1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+    // right face
+     1.0f,  1.0f,  1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f,  1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f,
+    // bottom face
+    -1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
+    -1.0f, -1.0f, -1.0f,
+    // top face
+    -1.0f,  1.0f, -1.0f,
+     1.0f,  1.0f , 1.0f,
+     1.0f,  1.0f, -1.0f,
+     1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f,  1.0f,
+  };
 
+
+  auto cube_vbo = std::make_unique<VBO>(sizeof(data), data, GL_STATIC_DRAW);
+
+  auto cube_vao = std::make_unique<VAO>();
+
+  cube_vao->set_vbo(*cube_vbo, 0U, 3U, 3U * sizeof(int), 0, GL_FLOAT);
+
+  cubemap_fbo->change_shader(std::make_unique<Shader>("../TinyRenderer/resource/shader/hdr2cubemap", "../TinyRenderer/resource/shader/hdr2cubemap"));
+
+  auto& cubemap_shader = cubemap_fbo->get_shader();
+
+  cubemap_shader->bind();
+  cubemap_shader->set_uniform("projection", proj);
+
+  glActiveTexture(GL_TEXTURE0);
+
+  glBindTexture(GL_TEXTURE_2D, hdr_id);
+
+  glViewport(0, 0, resolution, resolution);
+  cubemap_fbo->bind();
+
+  for (size_t i = 0; i < faces; i++) {
+    cubemap_shader->set_uniform("view", views[i]);
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, 
+                           GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, m_id, 0);
+
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    cube_vao->bind();
+
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    cube_vao->ubind();
+
+  }
+  
 }
 Texture::Texture(GLuint width, GLuint height) : m_target(GL_TEXTURE_2D) {
   m_width = width;
